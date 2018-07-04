f7df2c650459bf99890b5c2c006a22d3031bd914
rask <2083967667@qq.com>
2018-07-04 21:49:51 +0800

    over

--- MiniSmarty.class.php
+++ MiniSmarty.class.php
@@ -2,17 +2,34 @@
 
 class MiniSmarty
 {
+    private $tpl_var = array();
+
+    public function assign($name, $value)
+    {
+        $this->tpl_var[$name] = $value;
+    }
+
     public function display($tpl)
     {
+        //global $title;
+        ////global $content;
+
         //1.获取数据(模板内容)
-        $content=file_get_contents($tpl);
+        $text = file_get_contents($tpl);
         //var_dump($content);
         //echo $content; //html代码被解析丢弃
         //2.制定内容替换
-        str_replace("{","<?php echo",$content);
-        str_replace("?","?>",$content);
+        $text = str_replace('{$', "<?php echo \$this->tpl_var['", $text);
+        $text = str_replace('}', "'];?>", $text);
 
-        var_dump($content);
+
+        //var_dump($content);
+        //echo $content; //php脚本被当成字符串输出，不解析变量
+        //3.把替换后的字符串写成文件
+        file_put_contents($tpl . ".php", $text);
+
+        //4.引入混编文件
+        require_once $tpl . ".php";
     }
 
 }
--- MiniSmartyTest.php
+++ MiniSmartyTest.php
@@ -5,4 +5,6 @@ $content = "从前的火车、邮件慢，一生只够爱一个人。";
 require_once "MiniSmarty.class.php";
 
 $miniSmarty=new MiniSmarty();
+$miniSmarty->assign("title",$title);
+$miniSmarty->assign("content",$content);
 $miniSmarty->display("02tpl_S2.html");
